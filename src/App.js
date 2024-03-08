c8af607dc9240d587c0415a83a6f20e1dca1073f
SameerSundaray <sameersundaray2014@gmail.com>
2024-03-08 12:56:24 +0530

    Item added

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ function App() {
   return (
     <Fragment>
       <Header/>
-      <main>
-      <Meals/>
-      </main>
+      <main><Meals/></main>
     </Fragment>
   );
 }
--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,4 +1,7 @@
+import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
 import classes from './AvailableMeals.module.css';
+
 
 const DUMMY_MEALS = [
     {
@@ -30,12 +33,20 @@ const DUMMY_MEALS = [
 
 const AvailableMeals = () => {
     
-    const MealsList = DUMMY_MEALS.map(meal => <li>{meal.name}</li>);
-    return <section className = {classes.meals}>
-          <ul>
-            {MealsList}
-          </ul>
+    const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem 
+    key = {meal.id} 
+    name = {meal.name} 
+    description = {meal.description} 
+    price = {meal.price}
+    />));
+    return ( 
+    <section className = {classes.meals}>
+        <Card>
+           <ul>{mealsList}</ul>
+        </Card>
     </section>
-}
+    );
+};
 
 export default AvailableMeals;
